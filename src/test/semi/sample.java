fd5732bd05a1e34acb63011b7579a961a713c47a
Chanho Yoo <poi200325@gmail.com>
2019-12-11 14:29:01 +0900

    출력문 하나 더 추가

--- src/test/semi/sample.java
+++ src/test/semi/sample.java
@@ -4,5 +4,6 @@ public class sample {
 	public static void main(String[] args) {
 		System.out.println("git repository 테스트입니다.");
 		System.out.println("test2");
+		System.out.println("test3");
 	}
 }

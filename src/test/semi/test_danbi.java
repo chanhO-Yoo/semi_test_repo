2378aa3be039052a3f57aa936ee7d6e55f5c8fd3
user1 <user1@user1-PC>
2019-12-17 14:59:15 +0900

    단비- test_branch 테스트 커밋

--- src/test/semi/test_danbi.java
+++ src/test/semi/test_danbi.java
@@ -3,6 +3,6 @@ package test.semi;
 public class test_danbi {
 	public void test() {
 		System.out.println("danbi_branch test중입니다.");
-		System.out.println("ddddddddddddd");
+		System.out.println("단비- test_branch 테스트!!!!");
 	}
 }

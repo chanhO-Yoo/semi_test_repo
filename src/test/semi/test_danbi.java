0c73d2d110ece2110e8067d129e105f68d736bf3
user1 <user1@user1-PC>
2019-12-17 15:07:24 +0900

    단비- test_branch 테스트2 커밋

--- src/test/semi/test_danbi.java
+++ src/test/semi/test_danbi.java
@@ -4,5 +4,6 @@ public class test_danbi {
 	public void test() {
 		System.out.println("danbi_branch test중입니다.");
 		System.out.println("단비- test_branch 테스트!!!!");
+		System.out.println("단비- test_branch 테스트2!!!!");
 	}
 }
